abd540d9da6aaed5a5849438ad2e9515e3a49649
Joe Malebe <Joe.Malebe@standardbank.co.za>
2024-01-04 18:45:00 +0200

    add category item grid layout

--- app/src/main/java/com/example/learningportal/CourseCategory.kt
+++ app/src/main/java/com/example/learningportal/CourseCategory.kt
@@ -1,13 +1,13 @@
 package com.example.learningportal
 
-data class CourseCategory(val id: Int, val name: String, val image: Int) {
+data class CourseCategory(val id: Int, val name: String, val image: Int, val backgroundColor: Int = 0) {
     companion object {
         fun getCourseCategories(): List<CourseCategory> {
             return listOf(
-                CourseCategory(1, "Developing", R.drawable.btn_1),
-                CourseCategory(2, "Designing", R.drawable.btn_2),
-                CourseCategory(3, "AL and ML", R.drawable.btn_3),
-                CourseCategory(4, "Explore", R.drawable.btn_4)
+                CourseCategory(1, "Developing", R.drawable.btn_1, R.color.teal_700),
+                CourseCategory(2, "Designing", R.drawable.btn_2, R.color.purple_700),
+                CourseCategory(3, "AL and ML", R.drawable.btn_3, R.color.teal_200),
+                CourseCategory(4, "Explore", R.drawable.btn_4, R.color.purple_200)
             )
         }
     }
--- app/src/main/java/com/example/learningportal/ui/screens/DashboardScreen.kt
+++ app/src/main/java/com/example/learningportal/ui/screens/DashboardScreen.kt
@@ -1,44 +1,110 @@
 package com.example.learningportal.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.learningportal.CourseCategory
 import com.example.learningportal.R
 import com.example.learningportal.ui.theme.LearningPortalTheme
 
 @Composable
-fun DashboardScreen(navHostController: NavHostController = rememberNavController()) {
+fun DashboardScreen(
+    navHostController: NavHostController = rememberNavController(),
+    categoryList: List<CourseCategory> = CourseCategory.getCourseCategories()
+) {
 
-    Column {
-        UserProfilePic()
+    Column(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxWidth()) {
+        UserProfilePic(modifier = Modifier.wrapContentHeight())
         Text(
             text = stringResource(id = R.string.what_would_you_like),
-            style = MaterialTheme.typography.headlineLarge
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier.padding(top = 16.dp)
         )
+        val category = CourseCategory.getCourseCategories().first()
+
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(128.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            items(categoryList.size) {
+                CategoryItem(categoryList[it])
+            }
+        }
     }
 }
 
 @Composable
-private fun UserProfilePic() {
-    Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier) {
+private fun CategoryItem(category: CourseCategory, modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .padding(vertical = 16.dp)
+            .aspectRatio(1f)
+            .background(
+                color = colorResource(id = category.backgroundColor),
+                shape = RoundedCornerShape(32.dp)
+            )
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(vertical = 24.dp)
+        ) {
+            Image(
+                bitmap = ImageBitmap.imageResource(category.image),
+                contentDescription = category.name,
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier
+                    .wrapContentSize()
+            )
+            Text(
+                text = category.name,
+                modifier = Modifier.padding(top = 16.dp),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+        }
+
+    }
+}
+
+@Composable
+private fun UserProfilePic(modifier: Modifier = Modifier) {
+    Row(verticalAlignment = Alignment.CenterVertically, modifier = modifier) {
         Image(
             ImageBitmap.imageResource(R.drawable.user_1),
             contentDescription = "Logo",
-            modifier = Modifier.size(80.dp)
+            modifier = Modifier.wrapContentSize(),
         )
 
         Text(
@@ -54,5 +120,13 @@ private fun UserProfilePic() {
 fun DashboardScreenPreview() {
     LearningPortalTheme {
         DashboardScreen()
+    }
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun CategoryItemPreview() {
+    LearningPortalTheme {
+        CategoryItem(CourseCategory.getCourseCategories()[2])
     }
 }
